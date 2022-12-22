66318e2acf348f2be3a3803975f9107d2d97c1d8
taleb-khaddour <taleb.khaddour.20311@gmail.com>
2022-12-22 12:59:56 +0200

    extended hello

--- tasks.js
+++ tasks.js
@@ -1,54 +1,50 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
-  process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
+function startApp(name) {
+  process.stdin.resume()
+  process.stdin.setEncoding('utf8')
+  process.stdin.on('data', onDataReceived)
   console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  console.log('--------------------')
 }
-
 
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 
 function onDataReceived(text) {
-  if (text === 'quit\n' || text=='exit\n') {
-    quit();
-  }
-  else if(text === 'hello\n'){
-    hello();
-  }
-  else if (text=='help\n'){
-        help();
-  }
-  else{
-    unknownCommand(text);
+  if (text === 'quit\n' || text ==='exit\n') {
+    quit()
+  } else if (text === 'hello\n'  || text.split(" ")[0] === "hello") {
+    hello(text)
+  } else if (text === 'help\n') {
+    help()
+  } else {
+    unknownCommand(text)
   }
 }
 
+ 
 
 /**
  * prints "unknown command"
@@ -57,38 +53,40 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"')
 }
-
 
 /**
  * Says hello
  *
  * @returns {void}
  */
-function hello(){
-  console.log('hello!')
+function hello(data) {
+  data = data.replace('\n', '').trim()
+  const arry = data.split(' ')
+  if (arry[0] === 'hello') {
+    const secondword = arry.slice(1).join(' ');
+    console.log('hello ' + secondword + '!')
 }
-
-
+}
 /**
  * Exits the application
  *
  * @returns {void}
  */
-function quit(){
+function quit() {
   console.log('Quitting now, goodbye!')
-  process.exit();
+  process.exit()
 }
 
-
 //List all the possible commnds
-function help(){
-  console.log("quit or exit : to quit outside the program and stop it \n" 
-  +"hello: to recive you a message hello!")
-  
+function help() {
+  console.log(
+    'quit or exit : to quit outside the program and stop it \n' +
+      'hello: to recive you a message hello!',
+  )
 }
 
 // The following line starts the application
-startApp("taleb khaddour")
+startApp('taleb khaddour')
